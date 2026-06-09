403fa2588601e8308d75a6701ce3a1e51c96f414
armandomh123 <amanjarrezh19@gmail.com>
2026-06-08 18:59:50 -0700

    feat: implement player movement and add procedural asteroid spawning system

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+from asteroid import Asteroid
 from player import Player
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    asteroidField = AsteroidField()
     clock = pygame.time.Clock()
     dt = 0.0
+    
+    
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
@@ -19,8 +31,9 @@ def main():
                 return
         screen.fill("black")
 
-        
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
